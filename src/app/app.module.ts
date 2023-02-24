6a9887716dfe276145312d9b02153f59829c73fe
Daryn <daryn200150@gmail.com>
2023-02-25 00:47:43 +0600

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {AudioRecordingService} from "./service/audio-recording.service";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [AudioRecordingService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
