a815c2f16dea4a4d96f4eadd20a87dc7ab50c311
Daryn <daryn200150@gmail.com>
2023-03-06 03:29:32 +0600

    added waveform demo

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,6 +1,10 @@
-import {Component, ElementRef, OnDestroy, ViewChild} from "@angular/core";
-import { AudioRecordingService } from "./service/audio-recording.service";
-import { DomSanitizer } from "@angular/platform-browser";
+import {ChangeDetectorRef, Component, ElementRef, OnDestroy, ViewChild} from "@angular/core";
+import {AudioRecordingService} from "./service/audio-recording.service";
+import {DomSanitizer} from "@angular/platform-browser";
+import WaveSurfer from 'wavesurfer.js';
+import TimelinePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js';
+import Regions from 'wavesurfer.js/dist/plugin/wavesurfer.regions.min.js';
+
 
 @Component({
   selector: "my-app",
@@ -13,11 +17,17 @@ export class AppComponent implements OnDestroy {
   isRecording = false;
   recordedTime: any;
   blobUrl;
+  blob: Blob;
   teste;
 
-  constructor(
-    private audioRecordingService: AudioRecordingService,
-    private sanitizer: DomSanitizer
+
+  wave: WaveSurfer = null;
+  public graph = undefined;
+
+
+  constructor(private cdr: ChangeDetectorRef,
+              private audioRecordingService: AudioRecordingService,
+              private sanitizer: DomSanitizer
   ) {
     this.audioRecordingService
       .recordingFailed()
@@ -30,6 +40,7 @@ export class AppComponent implements OnDestroy {
       this.blobUrl = this.sanitizer.bypassSecurityTrustUrl(
         URL.createObjectURL(data.blob)
       );
+      this.blob = data.blob;
     });
   }
 
@@ -54,6 +65,61 @@ export class AppComponent implements OnDestroy {
       this.isRecording = false;
     }
   }
+
+  onPreviewPressed(): void {
+    if (!this.wave) {
+      this.generateWaveform();
+    }
+
+    this.cdr.detectChanges();
+
+    if (this.blob) {
+      console.log(" blob: ", this.blob);
+      Promise.resolve().then(() => this.wave.loadBlob(this.blob));
+    }
+  }
+
+  onStopPressed(): void {
+    this.wave.stop();
+  }
+
+  generateWaveform(): void {
+    Promise.resolve(null).then(() => {
+      this.wave = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'violet',
+        progressColor: 'purple',
+        plugins: [
+          TimelinePlugin.create({
+            container: '#wave-timeline'
+          }),
+          Regions.create({
+            regions: [
+              {
+                start: 1,
+                end: 3,
+                loop: true,
+                color: 'hsla(400, 100%, 30%, 0.5)'
+              }, {
+                start: 5,
+                end: 7,
+                loop: false,
+                color: 'hsla(200, 50%, 70%, 0.4)'
+              }
+            ],
+            dragSelection: {
+              slop: 5
+            }
+          })
+        ]
+      });
+
+      this.wave.on('ready', () => {
+        this.wave.play();
+      });
+    });
+  }
+
 
   clearRecordedData() {
     this.blobUrl = null;
